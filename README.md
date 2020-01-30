bd6c8c716bb3307c4b74e74dcec6c6a0985b1aeb
ivanavocode <47441854+ivanavocode@users.noreply.github.com>
2020-01-30 10:37:17 +0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Simple calculator
  React+Redux calculator  
+
+Install package - npm i --> Start project - npm start
---
+++ src/components/counter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'; 
+import * as actions from '../actions';
+
+const Counter = ({ counter, inc, dec, rnd }) => {
+  return (
+    <div className="jumbotron">
+      <h2>{counter}</h2>
+      <button onClick={inc}id="inc" className="btn btn-primary btn-lg">INC</button>
+      <button onClick={dec}id="dec" className="btn btn-primary btn-lg">DEC</button>
+      <button onClick={rnd}id="rnd" className="btn btn-primary btn-lg">RND</button>      
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state
+  };
+};
+
+// const mapDispatchToprops = (dispatch) => {
+
+//   return bindActionsCreators(actions, dispatch);
+
+// };
+
+export default connect(mapStateToProps, actions)(Counter);
